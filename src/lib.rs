3d70672ff599cc4e3b9f32be7da049e85e3916f1
Martin Faartoft <martin.faartoft@maersk.com>
2022-07-31 19:51:15 +0200

    populate bomb_neighbors

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use rand::prelude::*;
+use rand::seq::index::sample;
+use std::collections::HashSet;
 
 type Dim = u8;
 type BoardSize = u16;
@@ -6,7 +7,7 @@ type BoardSize = u16;
 pub struct Tile {
     pub is_bomb: bool,
     pub is_revealed: bool,
-    pub bomb_neighbors: u8
+    pub bomb_neighbors: u8,
 }
 
 pub enum GameState {
@@ -58,27 +59,69 @@ impl Game {
     }
 
     fn init_random_game_board(width: Dim, height: Dim, bomb_count: BoardSize) -> Vec<Tile> {
-        let mut rng = rand::thread_rng();
-        let bomb_indices = rand::seq::index::sample(&mut rng, (width * height) as usize, bomb_count as usize);
-        
-        // TODO: map bomb_indices to vector of tiles and return
-        let mut board: Vec<Tile> = Vec::new();
-        board.push(Tile {
-            is_bomb: bomb_count > 0,
-            is_revealed: false,
-            bomb_neighbors: 0
-        });
+        let size = (width * height) as usize;
+        let bombs: HashSet<usize> = sample(&mut rand::thread_rng(), size, bomb_count as usize)
+            .into_iter()
+            .collect();
 
-        return board;
-    }
-}
+        let mut bomb_count = vec![0; size];
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+        fn neighbour_indices(i: usize, width: usize, max: usize) -> Vec<usize> {
+            let on_left_edge = i % width == 0;
+            let on_top_row = i < width;
+            let on_right_edge = i % width == width - 1;
+            let on_bottom_row = max - i < width;
 
-    #[test]
-    fn single_tile_board_should_have_zero_bomb_neighbors() {
+            let mut neighbors = Vec::new();
 
+            if !on_top_row {
+                neighbors.push(i - width);
+                if !on_left_edge {
+                    neighbors.push(i - width - 1);
+                }
+
+                if !on_right_edge {
+                    neighbors.push(i - width + 1);
+                }
+            }
+
+            if !on_left_edge {
+                neighbors.push(i - 1);
+            }
+
+            if !on_right_edge {
+                neighbors.push(i + 1);
+            }
+
+            if !on_bottom_row {
+                neighbors.push(i + width);
+                if !on_left_edge {
+                    neighbors.push(i + width - 1);
+                }
+
+                if !on_right_edge {
+                    neighbors.push(i + width + 1);
+                }
+            }
+
+            return neighbors;
+        }
+
+        for i in 0..size {
+            let neighbors = neighbour_indices(i, width as usize, size);
+            bomb_count[i] += neighbors.into_iter().filter(|i| bombs.contains(i)).count() as u8;
+        }
+
+        let tiles = bomb_count
+            .into_iter()
+            .enumerate()
+            .map(|(i, count)| Tile {
+                is_bomb: bombs.contains(&i),
+                is_revealed: false,
+                bomb_neighbors: count,
+            })
+            .collect();
+
+        return tiles;
     }
 }
--- tests/lib_tests.rs
+++ tests/lib_tests.rs
@@ -19,3 +19,22 @@ fn game_with_single_non_bomb_tile_that_is_revealed_should_be_won() {
     game.reveal(0, 0);
     assert!(matches!(game.state, GameState::Won));
 }
+
+#[test]
+fn single_tile_board_should_have_zero_bomb_neighbors() {
+    let game = Game::new(1, 1, 0);
+    assert_eq!(game.board[0].bomb_neighbors, 0);
+}
+
+#[test]
+fn two_by_one_board_with_two_bombs_should_both_have_one_neighbor() {
+    let game = Game::new(2, 1, 2);
+    assert_eq!(game.board[0].bomb_neighbors, 1);
+    assert_eq!(game.board[1].bomb_neighbors, 1);
+}
+
+#[test]
+fn three_by_three_board_all_bombs_center_tile_should_have_eight_neighbors() {
+    let game = Game::new(3, 3, 9);
+    assert_eq!(game.board[4].bomb_neighbors, 8);
+}
