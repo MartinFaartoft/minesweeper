9e3edb7f46a223b31485bb5adf9ca2dabe99909b
Martin Faartoft <martin@ftft.dk>
2022-08-01 14:20:44 +0200

    rewrite logic to return new state after each reveal

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use rand::seq::index::sample;
 use std::collections::HashSet;
 
-type Dim = u8;
-type BoardSize = u16;
+type Dim = usize;
+type BoardSize = usize;
 
 pub struct Tile {
     pub is_bomb: bool,
@@ -11,117 +11,133 @@ pub struct Tile {
 }
 
 pub enum GameState {
-    InProgress,
-    Won,
-    Lost,
+    InProgress(Game),
+    Won(FinishedGame),
+    Lost(FinishedGame),
 }
 
 pub struct Game {
     pub width: Dim,
     pub height: Dim,
     pub board: Vec<Tile>,
-    pub state: GameState,
+}
+
+pub struct FinishedGame {
+    pub board: Vec<Tile>,
 }
 
 impl Game {
-    pub fn new(width: Dim, height: Dim, bomb_count: BoardSize) -> Self {
-        let board = Game::init_random_game_board(width, height, bomb_count);
-        let state = Game::calculate_state(&board);
-        Game {
-            width: width,
-            height: height,
-            board,
-            state,
-        }
+    pub fn new_random(width: Dim, height: Dim, bomb_count: BoardSize) -> GameState {
+        let bomb_indices: HashSet<usize> = sample(
+            &mut rand::thread_rng(),
+            (width * height) as usize,
+            bomb_count as usize,
+        )
+        .into_iter()
+        .collect();
+
+        Game::new(width, height, &bomb_indices)
     }
 
-    pub fn reveal(&mut self, x: Dim, y: Dim) {
+    pub fn new(width: Dim, height: Dim, bomb_indices: &HashSet<usize>) -> GameState {
+        let game = Game {
+            width,
+            height,
+            board: Game::init_game_board(width, height, &bomb_indices),
+        };
+
+        game.calculate_state()
+    }
+
+    pub fn reveal(mut self, x: Dim, y: Dim) -> GameState {
         let index = x + self.width * y;
 
         let mut tile = &mut self.board[index as usize];
         tile.is_revealed = true;
 
-        self.state = Game::calculate_state(&self.board);
+        Game::calculate_state(self)
     }
 
-    fn calculate_state(board: &Vec<Tile>) -> GameState {
-        for tile in board.iter() {
+    fn calculate_state(self) -> GameState {
+        let mut unrevealed_non_bomb = false;
+        for tile in &self.board {
             if tile.is_bomb && tile.is_revealed {
-                return GameState::Lost;
+                return GameState::Lost(FinishedGame { board: self.board });
             }
 
             if !tile.is_bomb && !tile.is_revealed {
-                return GameState::InProgress;
+                unrevealed_non_bomb = true;
             }
         }
 
-        GameState::Won
+        if unrevealed_non_bomb {
+            GameState::InProgress(self)
+        } else {
+            GameState::Won(FinishedGame { board: self.board })
+        }
     }
 
-    fn init_random_game_board(width: Dim, height: Dim, bomb_count: BoardSize) -> Vec<Tile> {
+    fn init_game_board(width: Dim, height: Dim, bomb_indices: &HashSet<usize>) -> Vec<Tile> {
         let size = (width * height) as usize;
-        let bombs: HashSet<usize> = sample(&mut rand::thread_rng(), size, bomb_count as usize)
-            .into_iter()
-            .collect();
-
         let mut bomb_count = vec![0; size];
 
-        fn neighbour_indices(i: usize, width: usize, max: usize) -> Vec<usize> {
-            let on_left_edge = i % width == 0;
-            let on_top_row = i < width;
-            let on_right_edge = i % width == width - 1;
-            let on_bottom_row = max - i < width;
-
-            let mut neighbors = Vec::new();
-
-            if !on_top_row {
-                neighbors.push(i - width);
-                if !on_left_edge {
-                    neighbors.push(i - width - 1);
-                }
-
-                if !on_right_edge {
-                    neighbors.push(i - width + 1);
-                }
-            }
-
-            if !on_left_edge {
-                neighbors.push(i - 1);
-            }
-
-            if !on_right_edge {
-                neighbors.push(i + 1);
-            }
-
-            if !on_bottom_row {
-                neighbors.push(i + width);
-                if !on_left_edge {
-                    neighbors.push(i + width - 1);
-                }
-
-                if !on_right_edge {
-                    neighbors.push(i + width + 1);
-                }
-            }
-
-            return neighbors;
-        }
-
         for i in 0..size {
-            let neighbors = neighbour_indices(i, width as usize, size);
-            bomb_count[i] += neighbors.into_iter().filter(|i| bombs.contains(i)).count() as u8;
+            let neighbors = Game::neighbour_indices(i, width as usize, size);
+            bomb_count[i] += neighbors
+                .into_iter()
+                .filter(|i| bomb_indices.contains(i))
+                .count() as u8;
         }
 
-        let tiles = bomb_count
+        bomb_count
             .into_iter()
             .enumerate()
             .map(|(i, count)| Tile {
-                is_bomb: bombs.contains(&i),
+                is_bomb: bomb_indices.contains(&i),
                 is_revealed: false,
                 bomb_neighbors: count,
             })
-            .collect();
+            .collect()
+    }
 
-        return tiles;
+    fn neighbour_indices(i: usize, width: usize, max: usize) -> Vec<usize> {
+        let on_left_edge = i % width == 0;
+        let on_top_row = i < width;
+        let on_right_edge = i % width == width - 1;
+        let on_bottom_row = max - i < width;
+
+        let mut neighbors = Vec::new();
+
+        if !on_top_row {
+            neighbors.push(i - width);
+            if !on_left_edge {
+                neighbors.push(i - width - 1);
+            }
+
+            if !on_right_edge {
+                neighbors.push(i - width + 1);
+            }
+        }
+
+        if !on_left_edge {
+            neighbors.push(i - 1);
+        }
+
+        if !on_right_edge {
+            neighbors.push(i + 1);
+        }
+
+        if !on_bottom_row {
+            neighbors.push(i + width);
+            if !on_left_edge {
+                neighbors.push(i + width - 1);
+            }
+
+            if !on_right_edge {
+                neighbors.push(i + width + 1);
+            }
+        }
+
+        neighbors
     }
 }
--- tests/lib_tests.rs
+++ tests/lib_tests.rs
@@ -1,40 +1,69 @@
 use minesweeper::*;
+use std::collections::HashSet;
+
+macro_rules! hash_set {
+    ( $( $x:expr ),* ) => {  // Match zero or more comma delimited items
+        {
+            let mut temp_set = HashSet::new();  // Create a mutable HashSet
+            $(
+                temp_set.insert($x); // Insert each item matched into the HashSet
+            )*
+            temp_set // Return the populated HashSet
+        }
+    };
+}
 
 #[test]
 fn game_with_unrevealed_non_bomb_tile_should_be_in_progress() {
-    let game = Game::new(1, 1, 0);
-    assert!(matches!(game.state, GameState::InProgress));
+    let state = Game::new_random(1, 1, 0);
+    assert!(matches!(state, GameState::InProgress(_)));
 }
 
 #[test]
 fn game_with_revealed_bomb_tile_should_be_lost() {
-    let mut game = Game::new(1, 1, 1);
-    game.reveal(0, 0);
-    assert!(matches!(game.state, GameState::Lost));
+    let state = Game::new(2, 1, &hash_set![0]);
+    let lost_game = to_in_progress(state).reveal(0, 0);
+    assert!(matches!(lost_game, GameState::Lost(_)));
 }
 
 #[test]
 fn game_with_single_non_bomb_tile_that_is_revealed_should_be_won() {
-    let mut game = Game::new(1, 1, 0);
-    game.reveal(0, 0);
-    assert!(matches!(game.state, GameState::Won));
+    let state = Game::new_random(1, 1, 0);
+    let won_game = to_in_progress(state).reveal(0, 0);
+    assert!(matches!(won_game, GameState::Won(_)));
 }
 
 #[test]
 fn single_tile_board_should_have_zero_bomb_neighbors() {
-    let game = Game::new(1, 1, 0);
-    assert_eq!(game.board[0].bomb_neighbors, 0);
+    let state = Game::new_random(1, 1, 0);
+    assert_eq!(to_in_progress(state).board[0].bomb_neighbors, 0);
 }
 
 #[test]
-fn two_by_one_board_with_two_bombs_should_both_have_one_neighbor() {
-    let game = Game::new(2, 1, 2);
+fn tile_with_one_bomb_neighbour_should_have_correct_neighbour_count() {
+    let state = Game::new(2, 1, &&hash_set![1]);
+    let game = to_in_progress(state);
     assert_eq!(game.board[0].bomb_neighbors, 1);
-    assert_eq!(game.board[1].bomb_neighbors, 1);
 }
 
 #[test]
 fn three_by_three_board_all_bombs_center_tile_should_have_eight_neighbors() {
-    let game = Game::new(3, 3, 9);
+    let state = Game::new(3, 3, &hash_set![0, 1, 2, 3, 5, 6, 7, 8]);
+    let game = to_in_progress(state);
     assert_eq!(game.board[4].bomb_neighbors, 8);
+}
+
+fn to_in_progress(s: GameState) -> Game {
+    match s {
+        GameState::InProgress(g) => g,
+        _ => panic!(),
+    }
+}
+
+fn _to_finished_game(s: GameState) -> FinishedGame {
+    match s {
+        GameState::Won(g) => g,
+        GameState::Lost(g) => g,
+        _ => panic!("State was InProgress"),
+    }
 }
